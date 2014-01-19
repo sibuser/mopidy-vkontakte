a6cb8d3c2d79b609a6d5d0550af57aa2b9328f7f
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-01-19 23:45:40 +0100

    Remove PlaybackProvider that does nothing

--- mopidy_vkontakte/actor.py
+++ mopidy_vkontakte/actor.py
@@ -20,13 +20,7 @@ class VKBackend(pykka.ThreadingActor, base.Backend):
         self.config = config
         self.session = VKSession(config=self.config)
         self.library = VKLibraryProvider(backend=self)
-        self.playback = VKPlaybackProvider(audio=audio, backend=self)
+        self.playback = base.BasePlaybackProvider(audio=audio, backend=self)
         self.playlists = VKPlaylistsProvider(backend=self)
 
         self.uri_schemes = ['vkontakte']
-
-
-class VKPlaybackProvider(base.BasePlaybackProvider):
-
-    def play(self, track):
-        return super(VKPlaybackProvider, self).play(track)
